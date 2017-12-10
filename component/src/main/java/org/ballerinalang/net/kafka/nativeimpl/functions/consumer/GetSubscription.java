28bcbe2ccb7423366699255ec58455ba318e9306
Kevin Ratnasekera <djkevincr@yahoo.com>
2017-12-10 05:32:22 +0530

    Add subscription native method

--- component/src/main/java/org/ballerinalang/net/kafka/nativeimpl/functions/consumer/GetSubscription.java
+++ component/src/main/java/org/ballerinalang/net/kafka/nativeimpl/functions/consumer/GetSubscription.java
@@ -16,8 +16,71 @@
 
 package org.ballerinalang.net.kafka.nativeimpl.functions.consumer;
 
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.KafkaException;
+import org.ballerinalang.bre.Context;
+import org.ballerinalang.bre.bvm.BLangVMErrors;
+import org.ballerinalang.model.types.TypeKind;
+import org.ballerinalang.model.values.BString;
+import org.ballerinalang.model.values.BStringArray;
+import org.ballerinalang.model.values.BStruct;
+import org.ballerinalang.model.values.BValue;
+import org.ballerinalang.natives.AbstractNativeFunction;
+import org.ballerinalang.natives.annotations.Argument;
+import org.ballerinalang.natives.annotations.BallerinaFunction;
+import org.ballerinalang.natives.annotations.Receiver;
+import org.ballerinalang.natives.annotations.ReturnType;
+import org.ballerinalang.net.kafka.Constants;
+import org.ballerinalang.util.exceptions.BallerinaException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
 /**
- * Created by djkevincr on 12/10/17.
+ * {@code }
  */
-public class GetSubscription {
+@BallerinaFunction(packageName = "ballerina.net.kafka",
+        functionName = "getSubscription",
+        receiver = @Receiver(type = TypeKind.STRUCT, structType = "KafkaConsumer",
+                structPackage = "ballerina.net.kafka"),
+        args = {
+                @Argument(name = "c",
+                        type = TypeKind.STRUCT, structType = "KafkaConsumer",
+                        structPackage = "ballerina.net.kafka")
+        },
+        returnType = {@ReturnType(type = TypeKind.ARRAY, elementType = TypeKind.STRING),
+                @ReturnType(type = TypeKind.STRUCT)},
+        isPublic = true)
+public class GetSubscription extends AbstractNativeFunction {
+    private static final Logger log = LoggerFactory.getLogger(GetTopicPartitions.class);
+
+    @Override
+    public BValue[] execute(Context context) {
+
+        BStruct consumerStruct = (BStruct) getRefArgument(context, 0);
+        String topic = getStringArgument(context, 0);
+
+        KafkaConsumer<byte[], byte[]> kafkaConsumer = (KafkaConsumer) consumerStruct
+                .getNativeData(Constants.NATIVE_CONSUMER);
+        if (kafkaConsumer == null) {
+            throw new BallerinaException("Kafka Consumer has not been initialized properly.");
+        }
+
+        try {
+            Set<String> subscriptions = kafkaConsumer.subscription();
+
+            BStringArray subscriptionArray = new BStringArray();
+            if (!subscriptions.isEmpty()) {
+                int i = 0;
+                for (String subscription : subscriptions) {
+                    subscriptionArray.add(i++, subscription);
+                }
+            }
+            return getBValues(subscriptionArray);
+        } catch (KafkaException e) {
+            return getBValues(null, BLangVMErrors.createError(context, 0, e.getMessage()));
+        }
+    }
+
 }
