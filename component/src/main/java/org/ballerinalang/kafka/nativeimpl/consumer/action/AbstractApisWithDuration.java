986d03dee9dcf2211e5e3af3dfbc949da97e828f
Thisaru Guruge <thisaru@wso2.com>
2018-10-31 20:26:09 +0530

    Add Abstract class to handle APIs with duration parameter

--- component/src/main/java/org/ballerinalang/kafka/nativeimpl/consumer/action/AbstractApisWithDuration.java
+++ component/src/main/java/org/ballerinalang/kafka/nativeimpl/consumer/action/AbstractApisWithDuration.java
@@ -1,5 +1,64 @@
 package org.ballerinalang.kafka.nativeimpl.consumer.action;
 
-public class AbstractApisWithDuration {
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.ballerinalang.bre.Context;
+import org.ballerinalang.model.NativeCallableUnit;
+import org.ballerinalang.model.values.BMap;
+import org.ballerinalang.model.values.BValue;
 
+import java.time.Duration;
+import java.util.Objects;
+import java.util.Properties;
+
+import static org.ballerinalang.kafka.util.KafkaConstants.NATIVE_CONSUMER;
+
+public abstract class AbstractApisWithDuration implements NativeCallableUnit {
+
+    private Context context;
+    protected static final long durationUndefinedValue = -1;
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    protected Duration getDurationFromLong(long value) {
+        if (value == -1) {
+            return null;
+        }
+        Duration duration = Duration.ofMillis(value);
+        return duration;
+    }
+
+    protected long getDefaultApiTimeout(BMap<String, BValue> consumerStruct) {
+        long duration;
+        Properties consumerProperties = (Properties) consumerStruct.getNativeData(NATIVE_CONSUMER);
+        if (isDefaultApiTimeoutDefined(consumerProperties)) {
+            String apiTimeoutValue = getDefaultApiTimeoutConsumerConfig(consumerProperties);
+            duration = Long.parseLong(apiTimeoutValue);
+        } else {
+            duration = -1;
+        }
+        return duration;
+    }
+
+    protected String getDefaultApiTimeoutConsumerConfig(Properties consumerProperties) {
+        String apiTimeoutValue = (String) consumerProperties.get(ConsumerConfig.DEFAULT_API_TIMEOUT_MS_CONFIG);
+        return apiTimeoutValue;
+    }
+
+    protected boolean isDefaultApiTimeoutDefined(Properties consumerProperties) {
+        Object defaultApiTimeout = consumerProperties.get(ConsumerConfig.DEFAULT_API_TIMEOUT_MS_CONFIG);
+        return Objects.nonNull(defaultApiTimeout);
+    }
+
+    @Override
+    public boolean isBlocking() {
+
+        return true;
+    }
 }
