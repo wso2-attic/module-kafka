23c58074b0b77c453bb8993e5a7b0b2abddabc2f
Thisaru Guruge <thisaru@wso2.com>
2018-11-15 13:32:14 +0530

    Add Kafka Service test

---
+++ component/src/test/java/org/ballerinalang/kafka/nativeimpl/consumer/KafkaConsumerServiceTest.java
@@ -0,0 +1,94 @@
+/*
+ *   Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinalang.kafka.nativeimpl.consumer;
+
+import io.debezium.kafka.KafkaCluster;
+import io.debezium.util.Testing;
+import org.ballerinalang.launcher.util.BCompileUtil;
+import org.ballerinalang.launcher.util.BRunUtil;
+import org.ballerinalang.launcher.util.BServiceUtil;
+import org.ballerinalang.launcher.util.CompileResult;
+import org.ballerinalang.model.values.BString;
+import org.ballerinalang.model.values.BValue;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Test cases for ballerina kafka consumer endpoint bind to a service .
+ */
+public class KafkaConsumerServiceTest {
+
+    private CompileResult compileResult;
+    private static File dataDir;
+    private static KafkaCluster kafkaCluster;
+
+    @BeforeClass
+    public void setup() throws IOException {
+        Properties prop = new Properties();
+        kafkaCluster = kafkaCluster().deleteDataPriorToStartup(true)
+                .deleteDataUponShutdown(true).withKafkaConfiguration(prop).addBrokers(1).startup();
+        kafkaCluster.createTopic("test", 1, 1);
+    }
+
+    @Test(description = "Test endpoint bind to a service")
+    public void testKafkaServiceEndpoint() {
+        String testString = "test_string";
+        compileResult = BCompileUtil.compileAndSetup("consumer/kafka_consumer_service.bal");
+        BServiceUtil.runService(compileResult);
+        BRunUtil.invokeStateful(compileResult, "funcKafkaProduce");
+        try {
+            // Wait till messages published and polled
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            // Do nothing
+        }
+        BValue[] returnBValues = BRunUtil.invokeStateful(compileResult, "funcKafkaGetResultText");
+        Assert.assertEquals(returnBValues.length, 1);
+        Assert.assertTrue(returnBValues[0] instanceof BString);
+        Assert.assertEquals(returnBValues[0].stringValue(), testString);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (kafkaCluster != null) {
+            kafkaCluster.shutdown();
+            kafkaCluster = null;
+            boolean delete = dataDir.delete();
+            // If files are still locked and a test fails: delete on exit to allow subsequent test execution
+            if (!delete) {
+                dataDir.deleteOnExit();
+            }
+        }
+    }
+
+    private static KafkaCluster kafkaCluster() {
+        if (kafkaCluster != null) {
+            throw new IllegalStateException();
+        }
+        dataDir = Testing.Files.createTestingDirectory("cluster-kafka-consumer");
+        kafkaCluster = new KafkaCluster().usingDirectory(dataDir).withPorts(2185, 9094);
+        return kafkaCluster;
+    }
+}
